52622b7c7e66b43e4d3fc2b9156ab7af60a2ed84
Shubham Sharma <101943611+Shubham2886@users.noreply.github.com>
2022-11-24 22:29:00 +0530

    Add files via upload

---
+++ Day_1/ProgramLinearQueue.java
@@ -0,0 +1,51 @@
+import java.util.*;
+
+public class ProgramLinearQueue {
+
+  public static void main(String args[]) {
+    int ch, e;
+    LinearQueue obj = new LinearQueue();
+    Scanner in = new Scanner(System.in);
+    System.out.println("Enter size of Queue:");
+    int s = in.nextInt();
+    obj.createQueue(s);
+    do {
+      System.out.println("1.Enqueue\n2.Dequeue\n3.print\n0.exit");
+      ch = in.nextInt();
+      switch (ch) {
+        case 1:
+          if (
+            !obj.isFull()
+          ) { //is not full
+            System.out.println("Enter Data:");
+            e = in.nextInt();
+            obj.enqueue(e);
+            System.out.println("Enqueue");
+          } else System.out.println("Queue Full");
+          break;
+        case 2:
+          if (
+            !obj.isEmpty()
+          ) { //is not empty
+            e = obj.dequeue();
+            System.out.println("Dequeue:" + e);
+          } else System.out.println("Queue Empty");
+          break;
+        case 3:
+          if (
+            !obj.isEmpty()
+          ) { //is not empty
+            System.out.println("Queue has:");
+            obj.printQueue();
+          } else System.out.println("Queue Empty");
+          break;
+        case 0:
+          System.out.println("Exiting ");
+          break;
+        default:
+          System.out.println("Wrong option selected");
+          break;
+      }
+    } while (ch != 0);
+  }
+}
